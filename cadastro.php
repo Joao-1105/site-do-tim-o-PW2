2771ea0401f2c0a7a9cdea77a58d3166fb1e2b36
Jota <jotavms577@gmail.com>
2025-09-19 15:01:03 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,15 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM aluno WHERE email = ? AND senha = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $email, $senha);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows === 1) {
-        $_SESSION['usuario'] = $email;
-        header("Location: pagina1.php");
+    $sql = "SELECT * FROM aluno WHERE email = :email AND senha = :senha";
+    $stmt = $pdo->prepare($sql);
+
+
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':senha', $senha);
+
+    $stmt->execute();
+
+    if ($stmt->rowCount() === 1) {
+        $_SESSION['usuario'] = $email; 
+        header("Location: pagina1.php");  
         exit;
     } else {
         $erro = "E-mail ou senha inválidos!";
@@ -39,14 +43,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="login-box">
             <h2>Login</h2>
             <?php if (isset($erro)) echo "<p class='erro'>$erro</p>"; ?>
-            <form method="POST" action="">
-                <input type="rm" name="rm" placeholder="RM" required>
-                <input type="text" name="Nome" placeholder="Nome" required>
-                <input type="email" name="email" placeholder="E-mail" required>
-                <input type="text" name="cpf" placeholder="CPF" required>
-                <button type="submit">Entrar</button>
+
+        <form method="POST" action="">
+        <!-- Campos de dados do usuário -->
+        <input type="text" name="rm" placeholder="RM" required>
+        <input type="text" name="nome" placeholder="Nome" required>
+        <input type="email" name="email" placeholder="E-mail" required>
+        <input type="password" name="senha" placeholder="Senha" required>
+        <input type="text" name="cpf" placeholder="CPF" required>
+        <button type="submit">Entrar</button>
+</form>
+
             </form>
         </div>
     </div>
 </body>
 </html>
+
+
