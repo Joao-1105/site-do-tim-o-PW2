5011aa1e09b5139f60b44c9b8a0af8785d5f7904
Jota <jotavms577@gmail.com>
2025-09-24 11:16:14 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,31 +1,13 @@
 <?php
-session_start();
-include("conexao.php"); 
+require 'Aluno.class.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+$aluno = new Aluno();
 
-
-    $sql = "SELECT * FROM aluno WHERE email = :email AND senha = :senha";
-    $stmt = $pdo->prepare($sql);
-
-
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':senha', $senha);
-
-    $stmt->execute();
-
-    if ($stmt->rowCount() === 1) {
-        $_SESSION['usuario'] = $email; 
-        header("Location: pagina1.php");  
-        exit;
-    } else {
-        $erro = "E-mail ou senha inválidos!";
-    }
+if(!$aluno){ 
+    echo "<h1>Banco indisponível. Tente mais tarde</h1>";
+    exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -38,26 +20,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="faixa-direita"></div>
 
     <div class="container">
-        <img class="logo" src="https://th.bing.com/th/id/R.6402dea91d23f6c1fdb88648939abbdb?rik=iN9K%2f3MHYFOONA&pid=ImgRaw&r=0" alt="Corinthians" width="120">
+        <img class="logo" src="https://th.bing.com/th/id/R.6402dea91d23f6c1fdb88648939abbdb?rik=iN9K%2f3MHYFOONA&pid=ImgRaw&r=0" 
+             alt="Corinthians" width="140">
 
         <div class="login-box">
             <h2>Login</h2>
-            <?php if (isset($erro)) echo "<p class='erro'>$erro</p>"; ?>
+             
 
-        <form method="POST" action="">
-        <!-- Campos de dados do usuário -->
-        <input type="text" name="rm" placeholder="RM" required>
-        <input type="text" name="nome" placeholder="Nome" required>
-        <input type="email" name="email" placeholder="E-mail" required>
-        <input type="password" name="senha" placeholder="Senha" required>
-        <input type="text" name="cpf" placeholder="CPF" required>
-        <button type="submit">Entrar</button>
-</form>
-
+            <form method="POST" action="aluno.php">
+                <input type="text" name="rm" placeholder="rm" required>
+                <input type="text" name="nome" placeholder="nome" required>
+                <input type="email" name="email" placeholder="email" required>
+                <input type="password" name="senha" placeholder="senha" required>
+                <input type="text" name="cpf" placeholder="cpf" required>
+                <button type="submit">Entrar</button>
             </form>
         </div>
     </div>
 </body>
 </html>
+
 
 
